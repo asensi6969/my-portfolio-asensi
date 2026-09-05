0a6445f34d743ee113517034a1164a086aa931de
Angelo D. Asensi <74574268+asensi6969@users.noreply.github.com>
2026-09-05 15:22:19 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,12 @@
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] }),
-    tailwindcss
+    tailwindcss(),
   ],
-  base: "/my-portfolio-asensi/"
-})
+
+  base: "/my-portfolio-asensi/",
+});
